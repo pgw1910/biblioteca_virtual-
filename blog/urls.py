7de11339f1af8b874f82156aa940db3f9da934eb
pgw1910 <pedroribeiroegj@gmail.com>
2025-09-18 10:48:57 -0300

    final

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index_view, name='index'),
+    path('', views.index_view, name='index'),
     path('login/', views.login_view, name='login'),
     path('acervo/', views.acervo_view, name='acervo'),
     path('detalhes/', views.detalhes_view, name='detalhes'),
