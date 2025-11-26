7c5daaf6a6174ee80f8db294046b773bae698ae1
pgw1910 <pedroribeiroegj@gmail.com>
2025-11-26 17:09:21 -0300

    a6

--- biblioteca/ugb/views.py
+++ biblioteca/ugb/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 
+def index_view(request):
+    return render(request, 'index.html')
+
+def login_view(request):
+    return render(request, 'login.html')
+
+def acervo_view(request):
+    return render(request, 'acervo.html')
+
+def detalhes_view(request):
+    return render(request, 'detalhes.html')
+
