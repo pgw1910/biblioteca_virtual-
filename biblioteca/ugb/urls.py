6505755f186022d6fa35ebed31885e48d3d29d50
pgw1910 <pedroribeiroegj@gmail.com>
2025-11-26 16:53:10 -0300

    a6

---
+++ biblioteca/ugb/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('index', views.index_view, name='index'),
+    path('login/', views.login_view, name='login'),
+    path('acervo/', views.acervo_view, name='acervo'),
+    path('detalhes/', views.detalhes_view, name='detalhes'),
+]
