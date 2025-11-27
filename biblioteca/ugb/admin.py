df0df9a8304e8a1ea17007489c1d69003e579317
pgw1910 <pedroribeiroegj@gmail.com>
2025-11-26 21:22:00 -0300

    a6

--- biblioteca/ugb/admin.py
+++ biblioteca/ugb/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import livros ,usuario
+admin.site.register(livros)
+admin.site.register(usuario)
--- biblioteca/ugb/models.py
+++ biblioteca/ugb/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class livros (models.Model):
+    Autor=models.CharField( max_length=50)
+    Titulo=models.CharField(max_length=50)
+    Publicao=models.IntegerField()
+    Categoria=models.CharField(max_length=50)
+    def __str__(self):
+        return self.Titulo
+    
+class usuario(models.Model):
+    Nome=models.CharField( max_length=50)
+    Matricula=models.CharField(max_length=50)
+    Senha=models.IntegerField()
+    cargo=models.CharField(max_length=13)
+
+
+    
+class bibliotecario(models.Model):
+    Nome=models.CharField( max_length=50)
+    Senha=models.IntegerField()
+    
+
+
+
+
